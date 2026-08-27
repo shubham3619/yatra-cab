acf62b500a8778395cdffe617ff9c6518d1a9c87
Lalit-threely <lalit@threely.io>
2026-08-27 17:03:07 +0530

    Revert "refactor: remove the Discover page"
    This reverts commit 1f96076647e7619dc51bc5028f0e3c426fd7d40a.

--- apps/customer/src/App.jsx
+++ apps/customer/src/App.jsx
@@ -7,6 +7,7 @@ import Book from './pages/Book.jsx';
 import MyRides from './pages/MyRides.jsx';
 import RideDetail from './pages/RideDetail.jsx';
 import Profile from './pages/Profile.jsx';
+import Discover from './pages/Discover.jsx';
 import Rewards from './pages/Rewards.jsx';
 import Track from './pages/Track.jsx';
 
@@ -33,6 +34,7 @@ export default function App() {
       >
         <Route path="/" element={<Home />} />
         <Route path="/book" element={<Book />} />
+        <Route path="/discover" element={<Discover />} />
         <Route path="/rewards" element={<Rewards />} />
         <Route path="/rides" element={<MyRides />} />
         <Route path="/rides/:id" element={<RideDetail />} />
